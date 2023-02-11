76bac542aa9e5e8f6480d0c146638cf1b2158889
H459785 <shubham.goel@honeywell.com>
2023-02-12 01:53:39 +0530

    toggle dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import About from './component/About';
 import NavBar from './component/NavBar';
 import TextForm from './component/TextForm';
 function App() {
+  const [mode, setMode] = useState('light')
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark')
+     document.body.style.backgroundColor = '#042743'
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
     <>
-      <NavBar title="MyFirstApp" />
+      <NavBar title="MyFirstApp" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3'>
-      <TextForm  heading="Enter the text to analyse below:"/>
+      <TextForm  heading="Enter the text to analyse below:" mode={mode} />
       </div>
-    
     </>
   );
 }
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -12,28 +12,49 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleClearClick = () => {
+        console.log("Button clicked");
+        let newText = '';
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         console.log("handle Button clicked");
         setText(event.target.value);
+    }
+    const handleCapitaliseClick = () => {
+        const arr = text.split(" ");
+
+//loop through each element of the array and capitalize the first letter.
+
+
+for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+}
+
+const str2 = arr.join(" ");
+        setText(str2);
     }
 
     return (
         <>
 
-            <div className='container'>
+            <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
                 <div className="mb-3">
                     <label htmlFor="myBox" className="form-label">{props.heading}</label>
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{background: props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743' }} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpperClick}>Convert To Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLowerClick}>Convert To Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCapitaliseClick}>Capitalise Text</button>
             </div>
-            <div className='contaner my-3'>
+            <div className='contaner my-3' style={{color:props.mode==='dark'?'white':'#042743'}}>
                 <h3>Your Text Summary</h3>
                 <p>{text.split(" ").length} words and {text.length} characters.</p>
                 <p>It will take {.0008 * text.split(" ").length} minutes to read.</p>
                 <h3>Preview</h3>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Enter your text above to preview it here'}</p>
             </div>
         </>
     )
